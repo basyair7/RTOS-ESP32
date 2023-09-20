e1276e26dc9fe86701d1ae5ad41eee9255bf3101
Fathul <basyair.fathul@gmail.com>
2023-09-20 09:47:39 +0700

    edit program rtos-atmega328p example

--- rtos-atmega328p/src/main.cpp
+++ rtos-atmega328p/src/main.cpp
@@ -1,33 +1,58 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
+#include <task.h>
 
 #define pinLed1 10
 #define pinLed2 9
 
-// Ukuran tumpukan (stack size) untuk masing-masing program
-#define STACK_SIZE 128
+void(*funcRestart)(void) = 0; // self restart program
 
-int i = 0;
+// Ukuran tumpukan (stack size) untuk masing-masing program
+static uint16_t STACK_SIZE = 256; // 256 byte 
+
+// Handle Program
+TaskHandle_t handleProgram1, handleProgram2;
+
 // Deklarasi fungsi program
 void program1(void *pvParameter) {
   (void) pvParameter; // untuk menghindari warning tidak terpakai
-  for(;;) {
+  int i = 1;
+  while(true) {
     Serial.println(F("Task 1 Running"));
     digitalWrite(pinLed1, HIGH);
-    delay(100);
+    vTaskDelay(pdMS_TO_TICKS(100));
     digitalWrite(pinLed1, LOW);
-    delay(100);
+    vTaskDelay(pdMS_TO_TICKS(100));
+    if(i > 100) {
+      Serial.println(F("Task 1 Stopped"));
+      vTaskDelete(handleProgram1);
+    }
+    i++;
   }
 }
 
 void program2(void *pvParameter) {
   (void) pvParameter; // untuk menghindari warning tidak terpakai
-  for (;;) {
+  int i = 1;
+  while(true) {
     Serial.println(F("Task 2 Running"));
     digitalWrite(pinLed2, HIGH);
-    delay(1000);
+    vTaskDelay(pdMS_TO_TICKS(1000));
     digitalWrite(pinLed2, LOW);
-    delay(1000);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    if(i > 5) {
+      Serial.println(F("Task 2 Stopped"));
+      vTaskDelete(handleProgram2);
+    }
+    i++;
+  }
+}
+
+void RestartProgram(void *pvParameter) {
+  while(true) {
+    Serial.println(F("\n\n\n\nSelf Restart\n Bye...\n\n"));
+    vTaskDelay(pdMS_TO_TICKS(30000));
+    funcRestart();
   }
 }
 
@@ -38,9 +63,10 @@ void setup() {
   pinMode(pinLed1, OUTPUT);
   pinMode(pinLed2, OUTPUT);
 
-  // Buat task program1 dan program2
-  xTaskCreate(program1, "Program 1", STACK_SIZE, NULL, 1, NULL);
-  xTaskCreate(program2, "Program 2", STACK_SIZE, NULL, 2, NULL);
+  // Buat task tiap fungsi program
+  xTaskCreate(program1, "Program 1", STACK_SIZE, NULL, 1, &handleProgram1);
+  xTaskCreate(program2, "Program 2", STACK_SIZE, NULL, 2, &handleProgram2);
+  xTaskCreate(RestartProgram, "Restart Program", STACK_SIZE, NULL, 3, NULL);
 
   // Mulai scheduler
   vTaskStartScheduler();
